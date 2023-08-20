ed1dc4fd42f1a7bbfedd15da5c942b84ad7696d8
Amaburi <drsservant@gmail.com>
2023-08-20 20:58:55 +0700

    Dua

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -4,10 +4,30 @@ import { useFrame } from '@react-three/fiber'
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
 
 const Backdrop = () => {
+  const shadows = useRef();
   return (
-    <AccumulativeShadows>
+    <AccumulativeShadows
+      position={[0, 0, -0.14]}
+      ref={shadows}
+      temporal
+      frames={60}
+      alphaTest={0.80}
+      scale={10}
+      rotation={[Math.PI/ 2, 0 ,0]}
+    >
       <RandomizedLight
         amount={4}
+        radius={8}
+        intensity={0.50}
+        ambient={0.20}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight
+        amount={4}
+        radius={5}
+        intensity={0.50}
+        ambient={0.20}
+        position={[-5, -5, -9]}
       />
     </AccumulativeShadows>
   )
