269d9343f936b071818b9376728ae8ddafe96225
Amaburi <drsservant@gmail.com>
2023-08-20 23:25:33 +0700

    Done All

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -29,12 +29,43 @@ const Customizer = () => {
       case "filepicker":
         return <FilePicker file= {file} setFile={setFile} readFile={readFile}/>
       case "aipicker":
-        return <AIPicker/>  
+        return <AIPicker
+          prompt = {prompt}
+          setPrompt = {setPrompt}
+          generatingImg = {generatingImg}
+          handleSubmit = {handleSubmit}
+        />  
       default:
         return null;
     }
   }
+  
+  const handleSubmit = async (type) => {
+    if(!prompt) return alert("Please enter a prompt");
 
+    try {
+      setGeneratingImg(true);
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`)
+    } catch (error) {
+      alert(error)
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
+    }
+  }
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type];
 
@@ -50,10 +81,20 @@ const Customizer = () => {
         break;
       case "stylishShirt":
         state.isFullTexture = !activeFilterTab[tabName];
+        break;
       default:
         state.isLogoTexture = true;
         state.isFullTexture = false;
+        break;
     }
+
+    // after setting the tab, activeTab needs to be updated
+    setActiveFilterTab((prevState)=> {
+      return{
+        ...prevState,
+        [tabName]: !prevState[tabName]
+      }
+    })
   }
   const readFile = (type) => {
     reader(file)
@@ -97,8 +138,8 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick = {()=> {}}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick = {()=> handleActiveFilterTab(tab.name)}
               />
             ))}
           </motion.div>
