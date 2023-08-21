13a6c67acb8b801ee4e4d8e65eae7aa0394c9f50
Amaburi <drsservant@gmail.com>
2023-08-21 12:36:18 +0700

    preset colors added

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -11,7 +11,24 @@ const ColorPicker = () => {
         color={snap.color}
         disableAlpha
         presetColors={[
-          
+          "#00ff9e",
+          "#fbff00",
+          "#66672b",
+          "#00cdff",
+          "#ff5e70",
+          "#fc9faa",
+          "#f8001b",
+          "#ff49c0",
+          "#ff9d46",
+          "#31fc85",
+          "#ffdccb",
+          "#b76339",
+          "#38bbc4",
+          "#b856f2",
+          "#838383",
+          "#666566",
+          "#333233",
+          "#00fffd"
         ]}
         onChange={(color) => state.color = color.hex}
       />
